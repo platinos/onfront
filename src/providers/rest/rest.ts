b76808b7ab285ab04ecc66db376ca6fac241bd4e
nbiryukov <nbiryukov@aspirity.com>
2018-08-08 12:12:26 +0700

    update rest client

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,55 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import querystring from 'querystring';
 
 @Injectable()
 export class RestProvider {
   apiUrl = 'http://35.194.226.60:3002/api/v1/';
-  constructor(public http: HttpClient) {
 
-  }
-  getData(endpoint) {
-    return new Promise(resolve => {
-      this.http.get(this.apiUrl + endpoint).subscribe(data => {
-        resolve(data);
-      }, err => {
-        console.log(err);
-      });
-    });
-  }
+  constructor(public http: HttpClient) {}
 
-  addData(endpoint, data) {
-    console.log(data);
+  request(method: String, endpoint: String, data: Object = {}) {
     return new Promise((resolve, reject) => {
-      this.http.post(this.apiUrl +endpoint, data)
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => {
-          reject(err);
-        });
-    });
-  }
-  putData(endpoint, data) {
-    
-    
-    return new Promise((resolve, reject) => {
-      this.http.post(this.apiUrl + endpoint, data)
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => {
-          reject(err);
-        });
-    });
-    
-    }
-  deleteData(endpoint, data) {
-    return new Promise((resolve, reject) => {
-      this.http.post(this.apiUrl + endpoint, data)
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => {
-          reject(err);
-        });
+      // @ts-ignore
+      this.http[method](this.apiUrl + endpoint, data).subscribe(
+        res => resolve(res),
+        err => reject(err));
     });
   }
 
+  getData(endpoint: String, data: Object = {}) {
+    const stringifiedData = querystring.stringify(data);
+    return this.request('get', `${endpoint}?${stringifiedData}`);
+  }
+
+  addData(endpoint: String, data: Object = {}) {
+    return this.request('post', endpoint, data);
+  }
+
+  putData(endpoint: String, data: Object = {}) {
+    return this.request('post', endpoint, data);
+  }
+
+  deleteData(endpoint: String, data: Object = {}) {
+    return this.request('post', endpoint, data);
+  }
 }
