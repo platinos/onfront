7d35d13c2462f6d0e4f7b66bc5c99d3207af3c9c
nbiryukov <nbiryukov@aspirity.com>
2018-08-10 14:02:23 +0700

    sendcoin - fix errors

--- src/pages/sendcoin/sendcoin.ts
+++ src/pages/sendcoin/sendcoin.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, NavParams, ViewController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserData } from '../../providers/user-data';
 import { RestProvider } from '../../providers/rest/rest';
-import { AlertController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -18,32 +17,33 @@ export class SendcoinPage {
   walletid:any;
   balance:any;
   allWallets:any;
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams, 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
     public viewCtrl: ViewController,
     public user:UserData,
     private _FB: FormBuilder,
     private rp: RestProvider,
-    private alertCtrl: AlertController) {
-      this.form = this._FB.group({
-        'amount': ['', Validators.required],
-        'pass': ['', Validators.required],
-        'dest': ['', Validators.required]});
-      this.person = { name:"" , phone: "", userId: ""};
-      let paramData = this.navParams.get('data');
-    let amount = paramData.amount;
-    let recipient = paramData.destAddress;
-        console.log("got values: "+amount+recipient);
-        
-        this.form.controls['amount'].setValue(amount);
-        this.form.controls['dest'].setValue(recipient);
-      
+    private alertCtrl: AlertController,
+  ) {
+    this.form = this._FB.group({
+      amount: ['', Validators.required],
+      pass: ['', Validators.required],
+      dest: ['', Validators.required],
+    });
+    this.person = { name: '' , phone: '', userId: '' };
 
+    const paramData = this.navParams.get('data');
+    const amount = paramData.amount;
+    const recipient = paramData.destAddress;
+    console.log('got values: ' + amount + recipient);
+
+    this.form.controls['amount'].setValue(amount);
+    this.form.controls['dest'].setValue(recipient);
   }
 
-
   ionViewDidLoad() {
-    this.user.getPhone().then((phone) => { 
+    this.user.getPhone().then((phone) => {
       this.person.phone = phone;
     });
     this.user.getUsername().then((userName) => {
@@ -54,13 +54,12 @@ export class SendcoinPage {
       this.getWallet();
     });
     this.getAllWallets();
-
-    
   }
+
   getAllWallets(){
-    this.rp.getData('wallet/getWallets/all/v1/v2').then(data=>{
-        this.allWallets = data;
-        this.allWallets = this.allWallets.response;
+    this.rp.getData('wallet/getWallets/all/v1/v2').then(data => {
+      this.allWallets = data;
+      this.allWallets = this.allWallets.response;
     });
   }
 
@@ -68,7 +67,7 @@ export class SendcoinPage {
     this.rp.getData('wallet/' + this.person.userId).then(data => {
       this.walletData = data;
       this.walletid = this.walletData.response.walletId;
-      //if(this.walletid!=undefined){
+      // if(this.walletid!=undefined){
       this.rp.getData('wallet/tbtc/' + this.walletid).then(data1 => {
         this.balancedata = data1;
         this.balance = this.balancedata.response.balance;
@@ -76,111 +75,106 @@ export class SendcoinPage {
       });
     });
   }
-  sendCoin(){
-    let sendingAlert = this.alertCtrl.create({
+
+  sendCoin() {
+    const sendingAlert = this.alertCtrl.create({
       title: 'Initiating Transfer',
-      subTitle: '<img src="assets/imgs/loading.gif"><br>Please wait.'
+      subTitle: '<img src="assets/imgs/loading.gif" /><br />Please wait.',
     });
-  
+
     sendingAlert.present();
-    let
-      dest:any = this.form.controls['dest'].value,
-      pass:any = this.form.controls['pass'].value,
-      amount:any = parseFloat(this.form.controls['amount'].value)*100000000;
-    let payLoad: any = {
-      "walletId": this.walletid,
-      "destAddress": dest,
-      "passphrase": pass,
-      "amount": amount
+    const dest:any = this.form.controls['dest'].value;
+    const pass:any = this.form.controls['pass'].value;
+    const amount:any = parseFloat(this.form.controls['amount'].value) * 100000000;
+    const payLoad: any = {
+      amount,
+      walletId: this.walletid,
+      destAddress: dest,
+      passphrase: pass,
     };
     console.log(payLoad);
-    
-          if(this.balance < amount){
-            sendingAlert.dismiss();
-            this.presentAlert();
-          }
-          else{
-            this.rp.addData('wallet/makeTransaction/type', payLoad).then(data => {                
-                console.log(data);
-                let temp:any = data;
-              sendingAlert.dismiss();
-                if(temp.error===undefined)
-                {
-                  this.successAlert(payLoad);
-                }
-                else{
-                  this.unsuccsessAlert(temp.error);
-                }
-              });
-          }
+
+    if (this.balance < amount) {
+      sendingAlert.dismiss();
+      this.presentAlert();
+    } else {
+      this.rp.addData('wallet/makeTransaction/type', payLoad).then((data) => {
+        console.log(data);
+        const temp: any = data;
+        sendingAlert.dismiss();
+        if (temp.error === undefined) {
+          this.successAlert(payLoad);
+        } else {
+          this.unsuccsessAlert(temp.error);
+        }
+      });
+    }
   }
 
   unsuccsessAlert(err){
-    let alert = this.alertCtrl.create({
+    const alert = this.alertCtrl.create({
       title: 'Transaction Failed',
       subTitle: err.result.error ,
-      buttons: ['Dismiss']
+      buttons: ['Dismiss'],
     });
     this.form.reset();
     alert.present();
   }
-  
-  successAlert(payLoad){
-    let alert = this.alertCtrl.create({
+
+  successAlert(payLoad) {
+    const alert = this.alertCtrl.create({
       title: 'Transaction Successful',
-      subTitle: 'Transfered '+ (parseFloat(payLoad.amount)/100000000) +' successfully.' ,
-      buttons: ['Dismiss']
+      subTitle: 'Transfered ' + (parseFloat(payLoad.amount) / 100000000) + ' successfully.',
+      buttons: ['Dismiss'],
     });
     this.form.reset();
     alert.present();
   }
   sendingAlert() {
-    
+
   }
 
   presentAlert() {
-    let alert = this.alertCtrl.create({
+    const alert = this.alertCtrl.create({
       title: 'Low Balance',
       subTitle: 'Please Maintain the balance for transfer',
-      buttons: ['Dismiss']
+      buttons: ['Dismiss'],
     });
     alert.present();
   }
-
 
   dismiss() {
     this.viewCtrl.dismiss();
   }
+
   presentPrompt() {
-    let alert = this.alertCtrl.create({
+    const alert = this.alertCtrl.create({
       title: 'choose a recipient',
-    
+
       buttons: [
         {
           text: 'Cancel',
           role: 'cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
+          handler: data => console.log('Cancel clicked')
         },
         {
           text: 'Next',
-          handler: data => {
+          handler: (data) => {
             console.log(data);
             this.setDestination(data);
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
     this.allWallets.forEach((item, index) => {
       alert.addInput({ type: 'radio', label: item.userId.name, value: item.addresses[0].address });
-      
+
     });
-    
 
     alert.present();
   }
+
   setDestination(destId){
-        this.form.controls['dest'].setValue(destId);
+    this.form.controls['dest'].setValue(destId);
   }
 }
