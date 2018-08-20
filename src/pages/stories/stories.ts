074e59470880bd63d7f6497158f9567d6431a29e
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2018-08-20 08:09:21 +0530

    qwerty

--- src/pages/stories/stories.ts
+++ src/pages/stories/stories.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, LoadingController, ViewController, AlertController  } from 'ionic-angular';
-import { CreatepostPage } from '../createpost/createpost';
-import { RestProvider } from '../../providers/rest/rest';
-import { User, UserData } from '../../providers/user-data';
+import {
+  Component
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ModalController,
+  LoadingController,
+  ViewController,
+  AlertController
+} from 'ionic-angular';
+import {
+  CreatepostPage
+} from '../createpost/createpost';
+import {
+  RestProvider
+} from '../../providers/rest/rest';
+import {
+  User,
+  UserData
+} from '../../providers/user-data';
 
 
 
@@ -13,26 +30,33 @@ import { User, UserData } from '../../providers/user-data';
   templateUrl: 'stories.html',
 })
 export class StoriesPage {
-  public person: User = { userId: '', name: '' , phone: '', pic: '', email: '' };
+  public person: User = {
+    userId: '',
+    name: '',
+    phone: '',
+    pic: '',
+    email: ''
+  };
   contentList: any;
   contents: any;
-  newComment:string = "";
+  projectList: any;
+  project: any;
+  newComment: string = "";
   userID: any;
-  feedpages='';
-  constructor(public navCtrl: NavController,     
-    public navParams: NavParams, 
+  feedpages = '';
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
     private alertCtrl: AlertController,
-    public modalCtrl: ModalController, 
+    public modalCtrl: ModalController,
     public restProvider: RestProvider,
-    public user:UserData,
+    public user: UserData,
     public loadingCtrl: LoadingController,
-    public viewCtrl: ViewController) {
-  }
+    public viewCtrl: ViewController) {}
 
   async ionViewWillEnter() {
     this.person = await this.user.getUser();
-    console.log(this.person);     
-    
+    console.log(this.person);
+
   }
 
   ionViewDidLoad() {
@@ -47,7 +71,9 @@ export class StoriesPage {
     });
   }
   presentPostModal(postId) {
-    this.navCtrl.push('PostsPage', { 'postId': postId });
+    this.navCtrl.push('PostsPage', {
+      'postId': postId
+    });
     // let createModal = this.modalCtrl.create();
     // createModal.present();
   }
@@ -57,9 +83,9 @@ export class StoriesPage {
     // TODO: remove try/catch when fix unlike request error on server
     try {
       await this.restProvider.putData(`content/${contentId}/${this.person.userId}`, {});
-      content.likesCount ++; 
+      content.likesCount++;
     } catch (e) {
-      content.likesCount --;
+      content.likesCount--;
     }
   }
 
@@ -69,23 +95,19 @@ export class StoriesPage {
   public share(contentId: string) {
     const content = this.getContentById(contentId);
     var message_temp = content.content;
-    if(content.image!=undefined)
-    { 
-      message_temp += '<img src='+content.image+' >';
+    if (content.image != undefined) {
+      message_temp += '<img src=' + content.image + ' >';
     }
 
 
     const alert = this.alertCtrl.create({
-      title: 'Sharing '+content.PostByUser.name+'\'s post',      
-      message:message_temp,
-      inputs: [
-        {
-          name: 'message',
-          placeholder: 'Write Something......',
-        },
-      ],
-      buttons: [
-        {
+      title: 'Sharing ' + content.PostByUser.name + '\'s post',
+      message: message_temp,
+      inputs: [{
+        name: 'message',
+        placeholder: 'Write Something......',
+      }, ],
+      buttons: [{
           text: 'Cancel',
           role: 'cancel',
           handler: data => console.log('Cancel clicked'),
@@ -94,9 +116,10 @@ export class StoriesPage {
           text: 'Share',
           handler: (data) => {
             // to call api
-            this.restProvider .addData('content/share/'+contentId+'/' + this.person.userId, {             
-              content: data.message, type:"post"
-            }).then((data) => {              
+            this.restProvider.addData('content/share/' + contentId + '/' + this.person.userId, {
+              content: data.message,
+              type: "post"
+            }).then((data) => {
               console.log(data);
               this.reloadStories();
             });
@@ -118,6 +141,11 @@ export class StoriesPage {
       .then(data => {
         this.contents = data;
         this.contentList = this.contents.response;
+      });
+    this.restProvider.getData('content/type/project')
+      .then(data => {
+        this.project = data;
+        this.projectList = this.project.response;
         setTimeout(() => {
           loading.dismiss();
         }, 0);
@@ -129,9 +157,16 @@ export class StoriesPage {
         this.contents = data;
         this.contentList = this.contents.response;
       });
+      this.restProvider.getData('content/type/project')
+      .then(data => {
+        this.project = data;
+        this.projectList = this.project.response;
+      });
   }
-  sendComment(contentId){
-    this.restProvider.addData('comment/addcomment/'+contentId+'/'+this.userID,{'comment': this.newComment})
+  sendComment(contentId) {
+    this.restProvider.addData('comment/addcomment/' + contentId + '/' + this.userID, {
+        'comment': this.newComment
+      })
       .then(data => {
         this.newComment = "";
         this.presentPostModal(contentId);
