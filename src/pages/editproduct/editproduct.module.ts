ee5d2cb6c0c40d3f886071bd8bdd2e60fd441d3f
Priyank Upadhyay <priyank@platinos.in>
2018-08-25 01:31:38 +0530

    qwerty

---
+++ src/pages/editproduct/editproduct.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditproductPage } from './editproduct';
+
+@NgModule({
+  declarations: [
+    EditproductPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditproductPage),
+  ],
+})
+export class EditproductPageModule {}
