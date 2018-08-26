cc43e8e677a275f5c5f7d9f6e33d73124022cf05
imran-elezire <32159477+imran-elezire@users.noreply.github.com>
2018-08-27 04:13:06 +0530

    qwerty

--- src/pages/editpost/editpost.ts
+++ src/pages/editpost/editpost.ts
@@ -1,5 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  Component
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ViewController
+} from 'ionic-angular';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import {
+  RestProvider
+} from '../../providers/rest/rest';
+import {
+  UserData
+} from '../../providers/user-data';
+import * as firebase from 'firebase';
+import {
+  ImageuploaderProvider
+} from '../../providers/imageuploader/imageuploader';
 
 /**
  * Generated class for the EditpostPage page.
@@ -14,12 +36,108 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'editpost.html',
 })
 export class EditpostPage {
+  public form: FormGroup;
+  storyImage: any;
+  iddata:any;
+  contentData: any;
+  datacheck:any;
+  contentList: any;
+  contents: any;
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    private _FB: FormBuilder,
+    public rs: RestProvider,
+    public user: UserData,    
+    private _IMG: ImageuploaderProvider, ) {
+    this.form = this._FB.group({
+      'contentText': ['', Validators.required]
+    });
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ionViewWillEnter() {
+    this.iddata = this.navParams.get('iddata');
+    console.log(this.iddata);
+    this.getdata();
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditpostPage');
+  }
+  dismiss() {;
+    this.viewCtrl.dismiss();
+  }
+
+  getdata() {   
+    this.rs.getData('content/'+this.iddata)
+      .then(data => {
+        this.contents = data;
+        this.contentList = this.contents.response;
+        this.form.controls['contentText'].setValue(this.contentList.content);
+        this.storyImage=this.contentList.image;
+      });
+
+  }
+
+  postContent() {
+    
+    
+      let contentText: any = this.form.controls['contentText'].value;
+      if (undefined !== this.storyImage) {
+        this.uploadImage(this.storyImage)
+          .then((snapshot: any) => {
+            snapshot.ref.getDownloadURL().then(downloadURL => {
+              let uploadedImage: any = downloadURL;
+              console.log(uploadedImage);
+              this.rs.putData('content/' + this.iddata, {
+                content: contentText,
+                image: uploadedImage,
+                type: "post"
+              }).then(data => {
+                this.dismiss();
+              });
+            });
+          });
+      } else {
+        this.rs.putData('content/' + this.iddata, {
+          content: contentText,
+          type: "post"
+        }).then(data => {
+          this.dismiss();
+        });
+
+      }
+
+
+  }
+
+  uploadImage(imageString): Promise < any > {
+    let image: string = 'movie-' + new Date().getTime() + '.jpg',
+      storageRef: any,
+      parseUpload: any;
+
+    return new Promise((resolve, reject) => {
+      storageRef = firebase.storage().ref('posters/' + image);
+      parseUpload = storageRef.putString(imageString, 'data_url');
+      parseUpload.on('state_changed', (_snapshot) => {
+          // console.log('snapshot progess ');
+          // console.log(_snapshot);
+        },
+        (_err) => {
+          reject(_err);
+        },
+        (success) => {
+          resolve(parseUpload.snapshot);
+        });
+    });
+  }
+
+  selectImage() {
+    this._IMG.selectImage()
+      .then((data) => {
+        this.storyImage = data;
+      });
   }
 
 }
--- src/pages/myposts/myposts.ts
+++ src/pages/myposts/myposts.ts
@@ -1,5 +1,28 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  Component
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ModalController,
+  LoadingController,
+  ViewController,
+  AlertController
+} from 'ionic-angular';
+import {
+  CreatepostPage
+} from '../createpost/createpost';
+import {
+  CreateprojectPage
+} from '../createproject/createproject';
+import {
+  RestProvider
+} from '../../providers/rest/rest';
+import {
+  User,
+  UserData
+} from '../../providers/user-data';
 
 /**
  * Generated class for the MypostsPage page.
@@ -14,12 +37,154 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'myposts.html',
 })
 export class MypostsPage {
+  public person: User = {
+    userId: '',
+    name: '',
+    phone: '',
+    pic: '',
+    email: ''
+  };
+  contentList: any;
+  contents: any;
+  projectList: any;
+  project: any;
+  product: any;
+  productList:any;
+  newComment: string = "";
+  userID: any;
+ 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    public modalCtrl: ModalController,
+    public restProvider: RestProvider,
+    public user: UserData,
+    public loadingCtrl: LoadingController,
+    public viewCtrl: ViewController) {}
+
+    async ionViewWillEnter() {
+      this.person = await this.user.getUser();
+      this.getStories();
+      //console.log(this.person);
+  
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MypostsPage');
+   // this.getStories();
   }
+
+  presentPostModal(postId) {
+    this.navCtrl.push('PostsPage', {
+      'postId': postId
+    });
+    // let createModal = this.modalCtrl.create();
+    // createModal.present();
+  }
+
+  async like(contentId: string) {
+    const content = this.getContentById(contentId);
+    // TODO: remove try/catch when fix unlike request error on server
+    try {
+      await this.restProvider.putData(`content/${contentId}/${this.person.userId}`, {});
+      content.likesCount++;
+    } catch (e) {
+      content.likesCount--;
+    }
+  }
+
+  private getContentById(contentId: string) {
+    return this.contentList.find(content => content.Id === contentId);
+  }
+  public share(contentId: string) {
+    const content = this.getContentById(contentId);
+    var message_temp = content.content;
+    if (content.image != undefined) {
+      message_temp += '<img src=' + content.image + ' >';
+    }
+    const alert = this.alertCtrl.create({
+      title: 'Sharing ' + content.PostByUser.name + '\'s post',
+      message: message_temp,
+      inputs: [{
+        name: 'message',
+        placeholder: 'Write Something......',
+      }, ],
+      buttons: [{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => console.log('Cancel clicked'),
+        },
+        {
+          text: 'Share',
+          handler: (data) => {
+            // to call api
+            this.restProvider.addData('content/share/' + contentId + '/' + this.person.userId, {
+              content: data.message,
+              type: "post"
+            }).then((data) => {
+              console.log(data);
+              this.reloadStories();
+            });
+          },
+        },
+      ],
+    });
+    alert.present();   
+
+
+  }
+  getStories() {
+    let loading = this.loadingCtrl.create({
+      content: '<img src="assets/imgs/loading.gif"/> <br>  Loading Stories.. Please wait.'
+    });
+    loading.setSpinner("none");
+    loading.present();
+    console.log(this.person);
+    this.restProvider.getData('content/user/'+this.person.userId+'/post')
+      .then(data => {
+        this.contents = data;
+        this.contentList = this.contents.response;
+        setTimeout(() => {
+          loading.dismiss();
+        }, 0);
+      });
+
+  }
+  reloadStories() {
+    this.restProvider.getData('content/user/'+this.person.userId+'/post')
+      .then(data => {
+        this.contents = data;
+        this.contentList = this.contents.response;
+        console.log(this.contentList);
+        
+      });
+  }
+  sendComment(contentId) {
+    this.restProvider.addData('comment/addcomment/' + contentId + '/' + this.userID, {
+        'comment': this.newComment
+      })
+      .then(data => {
+        this.newComment = "";
+        this.presentPostModal(contentId);
+      });
+  }
+  doRefresh(refresher) {
+    setTimeout(() => {
+      this.reloadStories();
+      refresher.complete();
+    }, 2000);
+  }
+
+    public Edit(id:any) {
+    console.log(id);
+    let createModal = this.modalCtrl.create("EditpostPage",{iddata: id});
+    createModal.present();
+    createModal.onDidDismiss(data => {
+    this.reloadStories();    
+
+  });
+  }
+
 
 }
