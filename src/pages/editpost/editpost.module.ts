1ff34e18eb211478a97fdd97c639650ebcb6a0e0
Priyank Upadhyay <priyank@platinos.in>
2018-08-27 02:38:23 +0530

    qwerty

---
+++ src/pages/editpost/editpost.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditpostPage } from './editpost';
+
+@NgModule({
+  declarations: [
+    EditpostPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditpostPage),
+  ],
+})
+export class EditpostPageModule {}
